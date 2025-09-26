f16f446eca83ae534fea0ee0fb54c32bc53fd37c
Ghulam Habib <ghulamhabib.x@gmail.com>
2025-09-26 21:55:39 +0500

    Today learn recuresion but i am week at backtracking which i revise ageai

---
+++ SemDsa/Concepts/Recursion/problmes/printLinkListRevers.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int info;
+    node* next;
+    node(int info)
+    {
+        this->info=info;
+        this->next=0;
+    }
+    ~node()
+    {
+
+    }
+};
+void insertAtHead(node * &head,node * &tail,int info)
+{
+    node*temp=new node(info);
+    if(head==NULL)
+    {
+        head=temp;
+        tail=temp;
+    }
+    else{
+        temp->next=head;
+        head=temp;
+    }
+
+}
+void insertAtTail(node * &head,node * &tail, int info)
+{
+    node* newNode=new node(info);
+    if(head==NULL)
+    {
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+}
+void print(node * head)
+{
+    node* temp=head;
+    while (temp!=NULL)
+    {
+        cout<<temp->info<<" ";
+        /* code */
+        temp=temp->next;
+    }
+    cout<<endl;  
+}
+void printRevers(node * head)
+{
+    if(head!=NULL)
+    {
+        printRevers(head->next);
+        cout<<head->info<<" ";
+    }
+}
+int main()
+{
+    node * nod=new node(10);
+    node * head=nod;
+    node * tail=nod;
+    insertAtHead(head,tail,20);
+    insertAtHead(head,tail,30);
+    insertAtHead(head,tail,40);
+    insertAtTail(head,tail,22);
+    insertAtTail(head,tail,33);
+    insertAtTail(head,tail,44);
+    insertAtTail(head,tail,55);
+
+    print(head);
+    printRevers(head);
+
+
+    cout << "Hello World\n";
+    return 0;
+}

0de32c0509e9cdaa4a94f9da9307b1ae6baf5d1d
ghulamhabib364 <ghulamhabib161.gmail.com>
2025-10-20 18:43:40 +0500

    lab4

--- SelfDsa/Revise/template.cpp
+++ SelfDsa/Revise/template.cpp
@@ -1,8 +1,20 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    int num;
+    cout << "Enter a positive number: ";
+    cin >> num;
+
+    if (num < 0)
+        goto negative; // jump to label 'negative'
+
+    cout << "You entered a positive number." << endl;
+    return 0;
+
+negative: // label
+    cout << "You entered a negative number!" << endl;
+    cout << "enter \n";
     return 0;
 }

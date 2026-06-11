88c35093a8552217f4f5c502a6e12b504e1175d5
Ghulam Habib <ghulamhabib.se@gmail.com>
2026-06-11 12:21:58 +0500

    added new feature

--- test.cpp
+++ test.cpp
@@ -12,105 +12,73 @@ using namespace std;
 class A
 {
 };
-void sepRateZero(vector<int> &arr)
+bool isValid(vector<int> &alph, char ch, int k)
 {
-    int st = 0;
-    int n = arr.size();
-    int end = n - 1;
-    int back = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 26; i++)
     {
-        if (arr[i] == 0)
+        // cout << alph[i] << endl;
+        if (alph[i] < k)
         {
-            swap(arr[i], arr[back]);
-            back++;
+            return false;
         }
     }
+    return true;
 }
-
-int getZeroIndex(const vector<int> &arr, int value)
+int longestSubstring(string s, int k)
 {
-    int n = arr.size();
-    int index = -1;
-    for (int i = 0; i < n; i++)
+    int n = s.length();
+    int high = 0;
+    vector<int> alph(26, 0);
+    int low = 0;
+    int len = INT_MIN;
+    while (high < n)
     {
-        if (arr[i] == value)
+        char ch = s[high];
+        int index = ch - 'a';
+        // cout << ch << endl;
+        alph[index]++;
+        high++;
+        bool tr = true;
+        for (int i = low; i < high; i++)
         {
-            index = i;
-        }
-    }
-    return index;
-}
-int getNegIndex(const vector<int> &arr)
-{
-    int index = -1;
-    int i = 0;
-    while (i < arr.size() && arr[i] < 0)
-    {
-        index = i;
-        i++;
-    }
-    return index;
-}
-void habibSort(vector<int> &arr)
-{
-    int negIndex = getNegIndex(arr);
-    int zeroIndex = getZeroIndex(arr, 0);
-    int posIndex = zeroIndex + 1;
-    if (negIndex != -1)
-    {
-        int s = 0;
-        int e = negIndex;
-        while ()
-        {
-            swap(arr[s], arr[e]);
-            s++;
-            e--;
-        }
-    }
-    if (zeroIndex != -1)
-    {
-    }
-}
-void separateNeg(vector<int> &arr)
-{
-    int zeroIndex = getZeroIndex(arr, 0);
-    if (zeroIndex != -1)
-    {
-        // Move all negative numbers to the left of the zero
-        int negIndex = zeroIndex + 1;
-        for (int i = zeroIndex + 1; i < arr.size(); i++)
-        {
-            if (arr[i] < 0)
+            char c = s[i];
+            if (!isValid(alph, c, k))
             {
-                swap(arr[i], arr[negIndex]);
-                negIndex++;
+
+                tr = false;
+                break;
             }
         }
-        int zeroFor = 0;
-        for (int i = negIndex - 1; i > zeroIndex; i--)
+        // cout << "WHile tr" << endl;
+        while (tr)
         {
-            swap(arr[i], arr[zeroFor]);
-            zeroFor++;
+            cout << "Here";
+            char ch = s[high];
+            high++;
+            index = ch - 'a';
+            alph[index]++;
+            if (!isValid(alph, ch, k))
+            {
+                tr = false;
+                
+                alph[low]--;
+                low++;
+                break;
+            }
+            int curr = high - low + 1;
+            // cout << "I am curr : " << curr << endl;
+            len = max(curr, len);
         }
+        
     }
-}
-void print(const vector<int> &arr)
-{
-    for (int x : arr)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
+    return len;
 }
 int main()
 {
-    vector<int> arr = {0, 1, 0, 5, 6, 7, 0, -23, 9, 0, -1, 0, -3};
+    string s = "ababbc";
+    int k = 3;
+    int ans = longestSubstring(s, k);
+    cout << ans << endl;
 
-    ;
-    sepRateZero(arr);
-    print(arr);
-    separateNeg(arr);
-    print(arr);
     return 0;
 }

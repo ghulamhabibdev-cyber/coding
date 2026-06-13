ea8d2cf0593a88162004dcf14ce8514b672ac691
Ghulam Habib <ghulamhabib.x@gmail.com>
2026-06-14 02:18:37 +0500

    implement the matrix using linkList

---
+++ dataStructure/doublyLinkList.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <list>
+using namespace std;
+class Node
+{
+public:
+    Node *next;
+    Node *prev;
+    int data;
+};
+
+int main()
+{
+
+    return 0;
+}
---
+++ dataStructure/listMatrxi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <list>
+using namespace std;
+
+class Node
+{
+public:
+    int row;
+    int col;
+    Node *next;
+    Node *low;
+    Node(int data)
+    {
+        
+    }
+};
+
+int main()
+{
+
+    return 0;
+}

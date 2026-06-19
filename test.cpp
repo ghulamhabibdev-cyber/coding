58e4e2f78fc0d274fa9e5c1cc726a3596e4d04ba
Ghulam Habib <ghulamhabib.se@gmail.com>
2026-06-20 00:43:29 +0500

    imlement valid parenthesis using string

--- test.cpp
+++ test.cpp
@@ -1,43 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include <list>
 using namespace std;
-
-string mapWordWeights(vector<string> &words, vector<int> &weights)
+class BrowserHistory
 {
-    int n = words.size();
-    map<string, int> mp;
-    for (int i = 0; i < n; i++)
+    int curr;
+    int visitIndex;
+    vector<string>pages;
+public:
+    BrowserHistory(string homepage)
     {
-        string currWord = words[i];
-        int j = 0;
-        int len = currWord.length();
-        long long sum = 0;
-        while (j < len)
+        pages.push_back(homepage);
+        curr=visitIndex=0;
+    }
+
+    void visit(string url)
+    {
+        pages.push_back(url);
+        visitIndex++;
+        curr=visitIndex;
+    }
+
+    string back(int steps)
+    {
+        while(curr>0&&steps>0)
         {
-            char ch = currWord[j];
-            int index = ch - 'a';
-
-            sum += weights[index];
-            j++;
+            curr--;
+            steps--;
         }
-        mp[currWord] = sum;
+        return pages[curr];
     }
-    string res = "";
-    for (int i = 0; i < n; i++)
-    {
-        int sum = mp[words[i]];
-        int mod = sum % 26;
-        char ch = 'z' - mod;
-        res += ch;
-    }
-    return res;
-}
 
-int main()
-{
-    vector<string> words = {"abcd", "def", "xyz"};
-    vector<int> wts = {5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2};
-    string ans = mapWordWeights(words, wts);
-    cout << ans << endl;
+    string forward(int steps)
+    {
+        while(curr<visitIndex&&steps>0)
+        {
+            curr++;
+            steps--;
+        }
+        return pages[curr];
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
+int main() {
+    
+    return 0;
 }

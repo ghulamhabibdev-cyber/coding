eefc1d463d38d581d684384f2b1cf0405e0258bf
Ghulam Habib <ghulamhabib.se@gmail.com>
2026-08-07 15:24:11 +0500

    jouney towards development and dsa

---
+++ C#DSA/helloWorld/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+public class Solution
+{
+    public int LinearSearch(List<int> arr, int target)
+    {
+        int n = arr.Count;
+        if (n <= 0)
+        {
+            return -1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+public class HelloWorld
+{
+    public static void Print<T>(T val)
+    {
+        Console.WriteLine(val);
+    }
+    public static void IteratorPirnt<T>(T arr) where T : IEnumerable
+    {
+        foreach (var val in arr)
+        {
+            Console.WriteLine(val);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        List<int> arr = new List<int>() { 1, 2, 3, 4 };
+        int index = s.LinearSearch(arr, 2);
+        if (index != -1)
+        {
+
+            Print<string>("Number is Prenset : " + arr[index]);
+        }
+    }
+}
